2e0c485f090ce564a6898d330de558e1f2ecbbd4
willam011 <74393095+willam011@users.noreply.github.com>
2020-11-13 10:15:03 +0530

    Update and rename cm.mk to Lineage.mk

--- Lineage.mk
+++ Lineage.mk
@@ -1,7 +1,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := TB3_710F
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
@@ -9,7 +9,7 @@ $(call inherit-product, device/lenovo/TB3_710F/device_TB3_710F.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := TB3_710F
-PRODUCT_NAME := cm_TB3_710F
+PRODUCT_NAME := Lineage_TB3_710F
 PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := TB3_710F
 PRODUCT_MANUFACTURER := lenovo
